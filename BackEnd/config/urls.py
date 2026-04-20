ade1dabe684e28c60e83706802055da000913d57
Danial4452 <danialkozahmet0@gmail.com>
2026-04-21 00:29:22 +0500

    Save local changes before rebase

--- BackEnd/config/urls.py
+++ BackEnd/config/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
     path('api/', include('products.urls')),
-    path('api/', include('orders.urls')),
 ]
--- BackEnd/orders/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import OrderListView, OrderCancelView
-
-urlpatterns = [
-    path('orders/', OrderListView.as_view(), name='order-list'),
-    path('orders/<int:order_id>/cancel/', OrderCancelView.as_view(), name='order-cancel'),
-]
